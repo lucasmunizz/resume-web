23bf5d7f36472836d5308dc1958ad0b47c0fb456
Lucas Muniz <lucaslima1048@gmail.com>
2023-11-30 11:09:31 -0300

    change imports on app module, change btn-primary

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HomeComponent } from './home.component';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    BtnPrimaryComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [],
+  exports: [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
