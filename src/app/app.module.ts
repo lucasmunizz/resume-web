cc57ceab272fd248dc61b0ddb47f9e6a08a91f3a
Lucas Muniz <lucaslima1048@gmail.com>
2023-11-30 11:32:52 -0300

    create main animation and uptade imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './components/home/home.module';
-import { MainSectionComponent } from './shared/main-section/main-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainSectionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.component';
+import { MainSectionComponent } from 'src/app/shared/main-section/main-section.component';
+import { MainAnimationComponent } from 'src/app/shared/main-animation/main-animation.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     BtnPrimaryComponent,
+    MainSectionComponent,
+    MainAnimationComponent
   ],
   imports: [
     BrowserModule,
